4116daea683c5277db361f8a5b8db9fce117b557
RadostevAA <55791524+RadostevAA@users.noreply.github.com>
2021-11-16 19:06:36 +0300

    Создание новых файлов

--- main.c
+++ main.c
@@ -1,30 +1,16 @@
 #include <stdio.h>
 
-#include <string.h>
+#include "askname.h"
 
 int main(int argc, char **argv)
 
 {
 
-<<<<<<< HEAD
-    
-printf("Hello world!\n");
-char name[255];
-=======
 char first[255], last[255];
->>>>>>> a06663a38f13820b1aab874c5053456443db89db
 
-printf("Enter your first name: ");
+askname(first, last);
 
-fgets(first, 255, stdin);
-
-first[strlen(first)-1] = '\0'; /* remove the newline at the end */
-
-printf("Now enter your last name: ");
-
-gets(last); /* buffer overflow? what's that? */
-
-printf("Hello %s %s!\n", first, last);
+printf("Hello, %s %s!\n", first, last);
 
 return 0;
 
